70b15b5ef5507dfd51aed4caec0546f21ecd8243
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-10-03 13:59:59 +0200

    e handle generate for onprem app

--- script/generate_files.sh
+++ script/generate_files.sh
@@ -3,16 +3,12 @@
 template_dir="../templates"
 
 generate_file() {
-    if [ "$1" == "create-all" ] ; then
-      create_all=true
-      shift 1
-    fi
     data_file=$1
     template_dir=$2
     template_file=$3
     out_file=$(echo "$template_file" | sed "s#$template_dir/##" | sed "s#.j2##" )
     out_dir=${out_file%\/*}
-    if test -f "$out_file"  || $create_all ; then
+    if test -f "$out_file" ; then
       echo -n "Generating $out_file ... "
       if test ! -f "$out_dir" ; then
         mkdir -p "$out_dir"
