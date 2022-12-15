875131aeaa56297d0afb7f9de671ac6cff0e3dbf
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-12-15 14:33:40 +0100

    d - Oppdatert analyse

--- script/size-stats.py
+++ script/size-stats.py
@@ -6,13 +6,17 @@ import datetime
 from subprocess import Popen, PIPE
 
 from lib.repos import projects_info
+import time
+
+tag = "2022T2END"
 
 today = datetime.datetime.today().strftime('%Y-%m-%d')
-filename = f"{os.getcwd()}/analysis/cloc-{today}.csv"
+now_file = f"{os.getcwd()}/analysis/cloc-{today}.csv"
+change_file = f"{os.getcwd()}/analysis/cloc-change-{tag}-{today}.csv"
 
 
 def cloc_for(path):
-    cloc_command_string = 'cloc . --csv --vcs git --exclude-dir=dist,build,buildSrc,gradle --exclude-list-file=.clocignore --exclude-lang=JSON,XML --quiet | egrep -v "^$" | cut -d, -f1-5'
+    cloc_command_string = 'cloc --csv --vcs git --exclude-dir=dist,build,gradle --exclude-list-file=.clocignore --exclude-lang=JSON,XML --quiet .'
     cloc_command = Popen(cloc_command_string, shell=True, stdout=PIPE, cwd=path, text=True)
     if cloc_command.returncode:
         exit(cloc_command.returncode)
@@ -22,19 +26,50 @@ def cloc_for(path):
     return result[1:-1]  # fjerner header og SUM
 
 
-all_stats = []
+def cloc_change_since(path, tag):  #  --exclude-list-file=.clocignore fungerer ikke her ser det ut til
+    cloc_command_string = f'cloc --csv --vcs git --exclude-dir=dist,build,gradle --exclude-lang=JSON,XML --quiet --diff --git {tag} origin/HEAD'
+    cloc_command = Popen(cloc_command_string, shell=True, stdout=PIPE, cwd=path, text=True)
+    if cloc_command.returncode:
+        exit(cloc_command.returncode)
+    time.sleep(2)
+    result = []
+    for line in cloc_command.stdout:
+        result += [line.rstrip()]
+    return result  # fjerner header og SUM
+
+
+size_stats = []
 
 projects = projects_info(os.getcwd())
 for project in projects:
     cloc_stats = cloc_for(project["path"])
     for row in cloc_stats:
-        all_stats += [today + "," + project["path"] + "," + project["module"] + "," + project["type"] + "," + row]
+        size_stats += [today + "," + project["path"] + "," + project["module"] + "," + project["type"] + "," + row]
 
-if os.path.exists(filename):
-    os.remove(filename)
+if os.path.exists(now_file):
+    os.remove(now_file)
 
-with open(filename, 'w', encoding="UTF-16") as csvfile:  # UTF-16 because Excel is stupid
+with open(now_file, 'w', encoding="UTF-16") as csvfile:  # UTF-16 because Excel is stupid
     writer = csv.writer(csvfile, delimiter=';', dialect='excel')
     writer.writerow(["date", "repo", "module", "type", "files", "language", "blank lines", "comment lines", "code lines"])
-    for line in all_stats:
+    for line in size_stats:
+        writer.writerow(line.split(","))
+
+change_stats = []
+
+for project in projects:
+    cloc_stats = cloc_change_since(project["path"], tag)
+    for row in cloc_stats:
+        change_stats += [today + "," + project["path"] + "," + project["module"] + "," + project["type"] + "," + row]
+
+if os.path.exists(change_file):
+    os.remove(change_file)
+
+with open(change_file, 'w', encoding="UTF-16") as csvfile:  # UTF-16 because Excel is stupid
+    writer = csv.writer(csvfile, delimiter=';', dialect='excel')
+    writer.writerow(
+        ["date", "repo", "module", "type", "language", "== files", "!= files", "+ files", "- files", "== blank",
+         "!= blank", "+ blank", "- blank", "== comment", "!= comment", "+ comment", "- comment", "== code", "!= code",
+         "+ code", "- code"])
+    for line in change_stats:
         writer.writerow(line.split(","))
