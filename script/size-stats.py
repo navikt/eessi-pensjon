fe62e75a50523cfb2970a9e9fb600ac61402199a
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2023-01-05 19:41:01 +0100

    d - Justerer på size-stats-script

--- script/size-stats.py
+++ script/size-stats.py
@@ -3,32 +3,46 @@
 import os
 import csv
 import datetime
+import shutil
 
 from lib.size_data import size_data, change_data
 
-tag = "2022T2END"
-
 today = datetime.datetime.today().strftime('%Y-%m-%d')
-now_file = f"{os.getcwd()}/analysis/cloc-{today}.csv"
-change_file = f"{os.getcwd()}/analysis/cloc-change-{tag}-{today}.csv"
+tertial = "2022T3"
 
-if os.path.exists(now_file):
-    os.remove(now_file)
+size_file_wth_date = f"{os.getcwd()}/analysis/cloc-{today}.csv"
+size_file = f"{os.getcwd()}/analysis/cloc.csv"
+change_file = f"{os.getcwd()}/analysis/cloc-change-{tertial}.csv"
 
-with open(now_file, 'w', encoding="UTF-16") as csvfile:  # UTF-16 because Excel is stupid
+if os.path.exists(size_file_wth_date):
+    os.remove(size_file_wth_date)
+
+with open(size_file_wth_date, 'w', encoding="UTF-16") as csvfile:  # UTF-16 because Excel is stupid
     writer = csv.writer(csvfile, delimiter=';', dialect='excel')
-    writer.writerow(["date", "repo", "module", "type", "files", "language", "blank lines", "comment lines", "code lines"])
-    for line in size_data():
-        writer.writerow(line.split(","))
+    data = size_data()
+    columns = data[0].keys()
+    writer.writerow(columns)
+    for line in data:
+        row = []
+        for column in columns:
+            row += [line[column]]
+        writer.writerow(row)
+
+if os.path.exists(size_file):
+    os.remove(size_file)
+
+shutil.copy(size_file_wth_date, size_file)
 
 if os.path.exists(change_file):
     os.remove(change_file)
 
 with open(change_file, 'w', encoding="UTF-16") as csvfile:  # UTF-16 because Excel is stupid
     writer = csv.writer(csvfile, delimiter=';', dialect='excel')
-    writer.writerow(
-        ["date", "repo", "module", "type", "language", "== files", "!= files", "+ files", "- files", "== blank",
-         "!= blank", "+ blank", "- blank", "== comment", "!= comment", "+ comment", "- comment", "== code", "!= code",
-         "+ code", "- code"])
-    for line in change_data(tag):
-        writer.writerow(line.split(","))
+    data = change_data(tertial)
+    columns = data[0].keys()
+    writer.writerow(columns)
+    for line in data:
+        row = []
+        for column in columns:
+            row += [line[column]]
+        writer.writerow(row)
