a488460f1f1b85d1ba5d1b16b0fc94e3abbde300
mariampervez <mariam.pervez@nav.no>
2026-08-06 12:03:38 +0200

    r - Endrer error melding til warning når fnr mangler for avdode.

--- templates/src/main/kotlin/no/nav/eessi/pensjon/shared/api/ApiRequest.kt
+++ templates/src/main/kotlin/no/nav/eessi/pensjon/shared/api/ApiRequest.kt
@@ -115,7 +115,7 @@ data class ApiRequest(
 
         private fun populerAvdodPersonId(request: ApiRequest, avdodaktoerID: String?, kreverAvdod: Boolean = false): PersonInfo? {
             if (kreverAvdod && avdodaktoerID == null) {
-                logger.error("Mangler fnr for avdød")
+                logger.warning("Mangler fnr for avdød")
                 if (request.gjenny) {
                     return null
                 }
